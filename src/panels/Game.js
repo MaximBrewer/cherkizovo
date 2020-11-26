3f1ba9ea2ca499d75c7f7f8e8f016f862f1a1020
Максим Пивоваров <maxim@MacBook-Pro-Maksim.local>
2020-11-26 18:43:13 +0600

    sds

--- src/panels/Game.js
+++ src/panels/Game.js
@@ -34,17 +34,8 @@ const Game = ({ id, go, route, fetchedUser, userHasSeenIntro }) => {
 		e.preventDefault();
 
 	}
-	const moveDown = (e) => {
-		e.preventDefault();
-
-	}
-	const drop = (e) => {
-		e.preventDefault();
-
-	}
 	const rotate = (e) => {
 		e.preventDefault();
-
 	}
 
 	const canvasOuter = useRef(null);
@@ -52,47 +43,6 @@ const Game = ({ id, go, route, fetchedUser, userHasSeenIntro }) => {
 	const [canvas, setCanvas] = useState({
 		width: 288
 	})
-
-	// useEffect(() => {
-	// 	const ctx = canvas.current.getContext('2d');
-	// 	ctx.fillRect(0, 0, 100, 100);
-	// }, [])
-
-	// var ctx = canvas.getContext( '2d' );
-	// var W = 300, H = 600;
-	// var BLOCK_W = W / COLS, BLOCK_H = H / ROWS;
-
-	// // draw a single square at (x, y)
-	// function drawBlock( x, y ) {
-	// 	ctx.fillRect( BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1 , BLOCK_H - 1 );
-	// 	ctx.strokeRect( BLOCK_W * x, BLOCK_H * y, BLOCK_W - 1 , BLOCK_H - 1 );
-	// }
-
-	// // draws the board and the moving shape
-	// function render() {
-	// 	ctx.clearRect( 0, 0, W, H );
-
-	// 	ctx.strokeStyle = 'black';
-	// 	for ( var x = 0; x < COLS; ++x ) {
-	// 		for ( var y = 0; y < ROWS; ++y ) {
-	// 			if ( board[ y ][ x ] ) {
-	// 				ctx.fillStyle = colors[ board[ y ][ x ] - 1 ];
-	// 				drawBlock( x, y );
-	// 			}
-	// 		}
-	// 	}
-
-	// 	ctx.fillStyle = 'red';
-	// 	ctx.strokeStyle = 'black';
-	// 	for ( var y = 0; y < 4; ++y ) {
-	// 		for ( var x = 0; x < 4; ++x ) {
-	// 			if ( current[ y ][ x ] ) {
-	// 				ctx.fillStyle = colors[ current[ y ][ x ] - 1 ];
-	// 				drawBlock( currentX + x, currentY + y );
-	// 			}
-	// 		}
-	// 	}
-	// }
 
 	return (
 		<Panel id={id}>
